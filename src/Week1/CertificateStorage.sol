b6d1538653fc9c4654588b72200f2e57fa821215
cantinaverse <cantinaverse@gmail.com>
2025-07-17 09:28:19 -0400

    Feat: Create CertificateStorage Contract In src/Week1 Folder

---
+++ src/Week1/CertificateStorage.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title CertificateStorage
+ * @dev A decentralized certificate/credential storage system for issuing and verifying certificates
+ */
+contract CertificateStorage {}
