6354b2454af424af0d0c049e6c7da27acd464871
cantinaverse <cantinaverse@gmail.com>
2025-07-18 10:50:59 -0400

    Feat: Add struct Event

--- src/Week1/EventRSVP.sol
+++ src/Week1/EventRSVP.sol
@@ -6,10 +6,33 @@ pragma solidity ^0.8.19;
  * @dev An event RSVP system where people can register for events
  */
 contract EventRSVP {
-        enum RSVPStatus {
+    enum RSVPStatus {
         PENDING,
         CONFIRMED,
         CANCELLED,
         WAITLISTED
+    }
+
+     enum EventStatus {
+        UPCOMING,
+        ONGOING,
+        COMPLETED,
+        CANCELLED
+    }
+
+    struct Event {
+        uint256 id;
+        address organizer;
+        string title;
+        string description;
+        string location;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 maxAttendees;
+        uint256 confirmedCount;
+        uint256 waitlistCount;
+        bool requiresApproval;
+        EventStatus status;
+        uint256 createdAt;
     }
 }
