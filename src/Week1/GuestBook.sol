060cc6ad3cdb3b7f614fdf364ef4c76098711bb1
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-21 10:05:27 -0400

    Feat: Add GuestBook Contract Natspec Comments

--- src/Week1/GuestBook.sol
+++ src/Week1/GuestBook.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title Guestbook
+ * @dev A simple messaging/guestbook contract where users can leave public messages
+ */
 contract GuestBook {}
