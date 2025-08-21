b64e58c1d94950931ad6ac5f8b7924d1f302b97f
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-21 10:06:20 -0400

    Feat: Add Message struct

--- src/Week1/GuestBook.sol
+++ src/Week1/GuestBook.sol
@@ -5,4 +5,11 @@ pragma solidity ^0.8.19;
  * @title Guestbook
  * @dev A simple messaging/guestbook contract where users can leave public messages
  */
-contract GuestBook {}
+contract GuestBook {
+    struct Message {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 id;
+    }
+}
